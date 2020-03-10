62f27540aae733b6931245af844453d9c379adfb
marcetin <marcetin@gmail.com>
2020-03-10 19:30:20 +0100

    ini

---
+++ model/cms.go
@@ -0,0 +1,19 @@
+package model
+
+type DuoCMS struct {
+	Title string
+}
+
+type DuoCMSuser struct {
+	Id   string
+	Name string
+}
+
+type DuoCMSsettings struct {
+	Title string
+}
+
+type DuoCMSthings struct {
+	Name string
+	Type string
+}
